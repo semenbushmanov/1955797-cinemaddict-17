0a336102e9ab30711581d7cb1306816fc386fbd1
Semen Bushmanov <bush.sem@mail.ru>
2022-05-30 22:19:40 +0500

    add filters data structure

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import StatisticsView from './view/statistics-view.js';
 import ContentPresenter from './presenter/content-presenter.js';
 import FilmsModel from './model/films-model.js';
 import { CommentsModel } from './model/films-model.js';
+import { generateFilter } from './mock/filter.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
@@ -13,8 +14,10 @@ const filmsModel = new FilmsModel();
 const commentsModel = new CommentsModel();
 const contentPresenter = new ContentPresenter(siteMainElement, filmsModel, commentsModel);
 
+const filters = generateFilter(filmsModel.films);
+
 render(new UserProfileView(), siteHeaderElement);
-render(new NavigationView(), siteMainElement);
+render(new NavigationView(filters), siteMainElement);
 render(new StatisticsView(), siteFooterStatisticsElement);
 
 contentPresenter.init();
--- src/view/navigation-view.js
+++ src/view/navigation-view.js
@@ -1,15 +1,29 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-const createNavigationTemplate = () => (
-  `<nav class="main-navigation">
-    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-  </nav>`
-);
+const createNavigationTemplate = (filters) => {
+  const watchlistCount = filters.find((element) => element.name ==='watchlist').count;
+  const historyCount = filters.find((element) => element.name ==='history').count;
+  const favoritesCount = filters.find((element) => element.name ==='favorites').count;
+
+  return (
+    `<nav class="main-navigation">
+      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlistCount}</span></a>
+      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${historyCount}</span></a>
+      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favoritesCount}</span></a>
+    </nav>`
+  );
+};
 
 export default class NavigationView extends AbstractView {
+  #filters = null;
+
+  constructor(filters) {
+    super();
+    this.#filters = filters;
+  }
+
   get template() {
-    return createNavigationTemplate();
+    return createNavigationTemplate(this.#filters);
   }
 }
