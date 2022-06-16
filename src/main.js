29a718160f732984581a56f4d5ad09b0a9cb3dbb
Semen Bushmanov <bush.sem@mail.ru>
2022-06-16 15:34:11 +0500

    intoduce data adapter WIP

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import ContentPresenter from './presenter/content-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import FilmsModel from './model/films-model.js';
 import FilterModel from './model/filter-model.js';
+import FilmsApiService from './films-api-service.js';
+
+const AUTHORIZATION = 'Basic k78j9uot5na';
+const END_POINT = 'https://17.ecmascript.pages.academy/cinemaddict';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const siteFooterStatisticsElement = document.querySelector('.footer__statistics');
-const filmsModel = new FilmsModel();
+const filmsModel = new FilmsModel(new FilmsApiService(END_POINT, AUTHORIZATION));
 const filterModel = new FilterModel();
 const contentPresenter = new ContentPresenter(siteMainElement, filmsModel, filterModel);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, filmsModel);
--- src/model/films-model.js
+++ src/model/films-model.js
@@ -2,7 +2,17 @@ import Observable from '../framework/observable.js';
 import { generateFilmDescription } from '../mock/film-data.js';
 
 export default class FilmsModel extends Observable {
+  #filmsApiService = null;
   #films = Array.from({ length: 23 }, generateFilmDescription);
+
+  constructor(filmsApiService) {
+    super();
+    this.#filmsApiService = filmsApiService;
+
+    this.#filmsApiService.films.then((films) => {
+      console.log(films.map(this.#adaptToClient));
+    });
+  }
 
   get films() {
     return this.#films;
@@ -22,5 +32,42 @@ export default class FilmsModel extends Observable {
     ];
 
     this._notify(updateType, update);
+  };
+
+  #adaptToClient = (film) => {
+    const release = {...film['film_info']['release'],
+      releaseCountry: film['film_info']['release']['release_country'],
+    };
+
+    delete release['release_country'];
+
+    const filmInfo = {...film['film_info'],
+      alternativeTitle: film['film_info']['alternative_title'],
+      totalRating: film['film_info']['total_rating'],
+      ageRating: film['film_info']['age_rating'],
+      release: release,
+    };
+
+    delete filmInfo['alternative_title'];
+    delete filmInfo['total_rating'];
+    delete filmInfo['age_rating'];
+
+    const userDetails = {...film['user_details'],
+      alreadyWatched: film['user_details']['already_watched'],
+      watchingDate: film['user_details']['watching_date'],
+    };
+
+    delete userDetails['already_watched'];
+    delete userDetails['watching_date'];
+
+    const adaptedFilm = {...film,
+      filmInfo: filmInfo,
+      userDetails: userDetails,
+    };
+
+    delete adaptedFilm['film_info'];
+    delete adaptedFilm['user_details'];
+
+    return adaptedFilm;
   };
 }
